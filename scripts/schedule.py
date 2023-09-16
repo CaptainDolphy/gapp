beb534ab1f3d00d47812a76cd476cd6dfda322e2
CaptainDolphy <93467774+CaptainDolphy@users.noreply.github.com>
2023-09-16 19:50:13 +0200

    Update schedule.py
    Expired Cookie Handling

--- scripts/schedule.py
+++ scripts/schedule.py
@@ -1,20 +1,21 @@
 import json
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+import os.path
 
 url = "https://gevo.edookit.net/user/login"
 
-import os.path
-
 ##generovani cookies.json file, kdyz neexistuje
-if os.path.isfile('cookies.json') == False:
+
+def createCookiesJson(_headless):
     with sync_playwright() as p:
         ##open browser
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=_headless)
         context = browser.new_context()
         page = context.new_page()
         page.goto(url)
-        page.wait_for_url("https://gevo.edookit.net/")
+
+        page.wait_for_url("https://gevo.edookit.net/", timeout=900000)
 
         cookies = context.cookies()
         ##delani json filu, tohle tam musi byt jinak to nefunguje
@@ -25,14 +26,24 @@ if os.path.isfile('cookies.json') == False:
                 json.dump(cookiesJson, f)
         context.close()
 
-
-def getTimeTable(): 
+def getTimeTable(_headless): 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=_headless)
         context = browser.new_context(storage_state="cookies.json")
         page = context.new_page()
         page.goto(url)
 
+        ## Expired cookies handling, sometimes we can still log in
+        if (page.get_by_text("Přihlásit přes").is_visible()):
+            page.get_by_text("Přihlásit přes").click()
+            if (page.get_by_role("button", name="Google").is_visible()):
+                page.get_by_role("button", name="Google").click()
+                
+            os.remove('cookies.json')
+            if (page.url != url):
+                createCookiesJson()
+
+        ## Main TimeTable Getter
         html = page.inner_html('#prints-timetable')
         soup = BeautifulSoup(html, 'html.parser')
 
@@ -40,7 +51,6 @@ def getTimeTable():
         data = []
 
         for i in range(classes.__len__()):
-
             class_ = classes[i].text.replace('\xa0',' ').split('\n')
             class_info = list(filter(None,[s.lstrip() for s in class_]))
         
@@ -59,5 +69,10 @@ def getTimeTable():
             with open("schedule.json", "w") as f:
                 json.dump(data, f)
 
+
+
+if os.path.isfile('cookies.json') == False:
+    createCookiesJson(_headless=False)
+
 if os.path.isfile('cookies.json'):
-    getTimeTable()
+    getTimeTable(_headless=True)
